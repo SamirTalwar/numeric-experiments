31248d86dded67cab32ff7dffa1aaad789987dd6
Samir Talwar <samir@noodlesandwich.com>
2016-02-06 17:02:22 +0000

    Extract out the inverse square root tests into a behaviours trait.

---
+++ src/test/scala/com/noodlesandwich/numeric/InverseSquareRootBehaviours.scala
@@ -0,0 +1,26 @@
+package com.noodlesandwich.numeric
+
+import com.noodlesandwich.numeric.DoubleMatchers.beNaN
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FunSpec, Matchers}
+
+trait InverseSquareRootBehaviours extends Matchers { this: FunSpec =>
+  def inverseSquareRoot(function: Double => Double): Unit = {
+    it("calculates the inverse square root of an inverse square number") {
+      function(0.25) should be(2.0)
+    }
+
+    it("calculates an irrational inverse square root") {
+      function(0.15625) should be(2.52982212813 +- 0.0000001)
+    }
+
+    it("calculates the inverse square root of 0 as NaN") {
+      function(0) should beNaN
+    }
+
+    it("calculates the inverse square root of a negative number as NaN") {
+      function(-3) should beNaN
+    }
+  }
+}
--- src/test/scala/com/noodlesandwich/numeric/InverseSquareRootSpec.scala
+++ src/test/scala/com/noodlesandwich/numeric/InverseSquareRootSpec.scala
@@ -1,27 +1,12 @@
 package com.noodlesandwich.numeric
 
-import com.noodlesandwich.numeric.DoubleMatchers.beNaN
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FunSpec, Matchers}
 
 @RunWith(classOf[JUnitRunner])
-class InverseSquareRootSpec extends FunSpec with Matchers {
+class InverseSquareRootSpec extends FunSpec with Matchers with InverseSquareRootBehaviours {
   describe("the inverse square root function") {
-    it("calculates the inverse square root of an inverse square number") {
-      InverseSquareRoot(0.25) should be(2.0)
-    }
-
-    it("calculates an irrational inverse square root") {
-      InverseSquareRoot(0.15625) should be(2.52982212813 +- 0.0000001)
-    }
-
-    it("calculates the inverse square root of 0 as NaN") {
-      InverseSquareRoot(0) should beNaN
-    }
-
-    it("calculates the inverse square root of a negative number as NaN") {
-      InverseSquareRoot(-3) should beNaN
-    }
+    it should behave like inverseSquareRoot(InverseSquareRoot.apply)
   }
 }
